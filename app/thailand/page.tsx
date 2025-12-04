0cf35a60abd510422aca24fd72f80f7198ddd531
PP-2412 <parth.patel@kgpian.iitkgp.ac.in>
2025-12-04 22:19:43 +0530

    Create page.tsx

---
+++ app/thailand/page.tsx
@@ -0,0 +1,18 @@
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+import CountryHero from '@/components/Destinations/CountryHero'
+import ToursSection from '@/components/Destinations/ToursSection'
+import ResortsSection from '@/components/Destinations/ResortsSection'
+import { thailandData } from '@/components/Destinations/destinationsData'
+
+export default function ThailandPage() {
+  return (
+    <main>
+      <Navbar />
+      <CountryHero country={thailandData} />
+      <ToursSection tours={thailandData.tours} countryName={thailandData.name} />
+      <ResortsSection resorts={thailandData.resorts} countryName={thailandData.name} />
+      <Footer />
+    </main>
+  )
+}
