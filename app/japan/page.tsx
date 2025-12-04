ea310f7f8be03576211dab6dc9e62538cad5a291
PP-2412 <parth.patel@kgpian.iitkgp.ac.in>
2025-12-04 22:24:02 +0530

    Update page.tsx

--- app/japan/page.tsx
+++ app/japan/page.tsx
@@ -1,5 +1,5 @@
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+import Navbar from '@/components/shared/Navbar'
+import Footer from '@/components/shared/Footer'
 import CountryHero from '@/components/Destinations/CountryHero'
 import ToursSection from '@/components/Destinations/ToursSection'
 import ResortsSection from '@/components/Destinations/ResortsSection'
