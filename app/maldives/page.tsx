a078007c30941fa486dddbf94fcf14f0d3677e15
PP-2412 <parth.patel@kgpian.iitkgp.ac.in>
2025-12-04 22:20:32 +0530

    Create page.tsx

---
+++ app/maldives/page.tsx
@@ -0,0 +1,18 @@
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+import CountryHero from '@/components/Destinations/CountryHero'
+import ToursSection from '@/components/Destinations/ToursSection'
+import ResortsSection from '@/components/Destinations/ResortsSection'
+import { maldivesData } from '@/components/Destinations/destinationsData'
+
+export default function MaldivesPage() {
+  return (
+    <main>
+      <Navbar />
+      <CountryHero country={maldivesData} />
+      <ToursSection tours={maldivesData.tours} countryName={maldivesData.name} />
+      <ResortsSection resorts={maldivesData.resorts} countryName={maldivesData.name} />
+      <Footer />
+    </main>
+  )
+}
