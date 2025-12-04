6577d2ef7da3f5dc4d3980c033d85d66f733cfa5
PP-2412 <parth.patel@kgpian.iitkgp.ac.in>
2025-12-04 22:54:15 +0530

    Update page.tsx

--- app/indonesia/page.tsx
+++ app/indonesia/page.tsx
@@ -1,17 +1,47 @@
-import Navbar from '@/components/shared/Navbar'
-import Footer from '@/components/shared/Footer'
+'use client'
+
+import { Suspense } from 'react'
+import { useSearchParams } from 'next/navigation'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 import CountryHero from '@/components/Destinations/CountryHero'
 import ToursSection from '@/components/Destinations/ToursSection'
 import ResortsSection from '@/components/Destinations/ResortsSection'
 import { indonesiaData } from '@/components/Destinations/destinationsData'
 
+function IndonesiaContent() {
+  const searchParams = useSearchParams()
+  const tourId = searchParams.get('tour')
+  const resortId = searchParams.get('resort')
+
+  return (
+    <>
+      <CountryHero country={indonesiaData} />
+      <ToursSection 
+        tours={indonesiaData.tours} 
+        countryName={indonesiaData.name} 
+        initialTourId={tourId}
+      />
+      <ResortsSection 
+        resorts={indonesiaData.resorts} 
+        countryName={indonesiaData.name}
+        initialResortId={resortId}
+      />
+    </>
+  )
+}
+
 export default function IndonesiaPage() {
   return (
     <main>
       <Navbar />
-      <CountryHero country={indonesiaData} />
-      <ToursSection tours={indonesiaData.tours} countryName={indonesiaData.name} />
-      <ResortsSection resorts={indonesiaData.resorts} countryName={indonesiaData.name} />
+      <Suspense fallback={
+        <div className="pt-28 pb-20 min-h-screen flex items-center justify-center">
+          <p className="font-sans text-[#44618b]">Loading Indonesia experiences...</p>
+        </div>
+      }>
+        <IndonesiaContent />
+      </Suspense>
       <Footer />
     </main>
   )
