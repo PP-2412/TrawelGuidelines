acaa90ddfca9a28bd653289b306c3c1f3e2ee1e5
PP-2412 <parth.patel@kgpian.iitkgp.ac.in>
2025-12-04 22:53:33 +0530

    Update page.tsx

--- app/thailand/page.tsx
+++ app/thailand/page.tsx
@@ -1,17 +1,47 @@
-import Navbar from '@/components/shared/Navbar'
-import Footer from '@/components/shared/Footer'
+'use client'
+
+import { Suspense } from 'react'
+import { useSearchParams } from 'next/navigation'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 import CountryHero from '@/components/Destinations/CountryHero'
 import ToursSection from '@/components/Destinations/ToursSection'
 import ResortsSection from '@/components/Destinations/ResortsSection'
 import { thailandData } from '@/components/Destinations/destinationsData'
 
+function ThailandContent() {
+  const searchParams = useSearchParams()
+  const tourId = searchParams.get('tour')
+  const resortId = searchParams.get('resort')
+
+  return (
+    <>
+      <CountryHero country={thailandData} />
+      <ToursSection 
+        tours={thailandData.tours} 
+        countryName={thailandData.name} 
+        initialTourId={tourId}
+      />
+      <ResortsSection 
+        resorts={thailandData.resorts} 
+        countryName={thailandData.name}
+        initialResortId={resortId}
+      />
+    </>
+  )
+}
+
 export default function ThailandPage() {
   return (
     <main>
       <Navbar />
-      <CountryHero country={thailandData} />
-      <ToursSection tours={thailandData.tours} countryName={thailandData.name} />
-      <ResortsSection resorts={thailandData.resorts} countryName={thailandData.name} />
+      <Suspense fallback={
+        <div className="pt-28 pb-20 min-h-screen flex items-center justify-center">
+          <p className="font-sans text-[#44618b]">Loading Thailand experiences...</p>
+        </div>
+      }>
+        <ThailandContent />
+      </Suspense>
       <Footer />
     </main>
   )
