f5629daf4280d65e0e869199a7e81dbbcf5bdcde
PP-2412 <parth.patel@kgpian.iitkgp.ac.in>
2025-12-04 22:21:28 +0530

    Create page.tsx

---
+++ app/japan/page.tsx
@@ -0,0 +1,18 @@
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+import CountryHero from '@/components/Destinations/CountryHero'
+import ToursSection from '@/components/Destinations/ToursSection'
+import ResortsSection from '@/components/Destinations/ResortsSection'
+import { japanData } from '@/components/Destinations/destinationsData'
+
+export default function JapanPage() {
+  return (
+    <main>
+      <Navbar />
+      <CountryHero country={japanData} />
+      <ToursSection tours={japanData.tours} countryName={japanData.name} />
+      <ResortsSection resorts={japanData.resorts} countryName={japanData.name} />
+      <Footer />
+    </main>
+  )
+}
