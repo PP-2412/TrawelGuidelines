3d99205e4e890f69db7ba2a86aafe5485f215432
PP-2412 <parth.patel@kgpian.iitkgp.ac.in>
2025-12-05 22:59:25 +0530

    Update page.tsx

--- app/vietnam/page.tsx
+++ app/vietnam/page.tsx
@@ -5,6 +5,7 @@ import { useSearchParams } from 'next/navigation'
 import Navbar from '@/components/shared/Navbar'
 import Footer from '@/components/shared/Footer'
 import CountryHero from '@/components/Destinations/CountryHero'
+import CreateYourOwn from '@/components/Destinations/CreateYourOwn'
 import ToursSection from '@/components/Destinations/ToursSection'
 import ResortsSection from '@/components/Destinations/ResortsSection'
 import { vietnamData } from '@/components/Destinations/destinationsData'
@@ -17,6 +18,7 @@ function VietnamContent() {
   return (
     <>
       <CountryHero country={vietnamData} />
+      <CreateYourOwn countryName={vietnamData.name} />
       <ToursSection 
         tours={vietnamData.tours} 
         countryName={vietnamData.name} 
